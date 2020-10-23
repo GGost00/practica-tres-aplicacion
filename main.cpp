b66246d7a3f34888a0304bb0ba76b66cd7188c7c
Gabriel.restrepoi <gabriel.restrepoi@udea.ude.co>
2020-10-23 12:05:59 -0500

    creado nuevo repositorio

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opcion;
+
+    cout<<"****** BIENBENIDO A SU CAJERO VIRTUAL ******"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"*   1.Primer metodo                        *"<<endl;
+    cout<<"*   2.Segundo metodo                       *"<<endl;
+    cout<<"*   3.Ningun metodo                        *"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"********************************************"<<endl;
+    cin>>opcion;
+    return 0;
+}
